6287edcfe4891dc92ffb8d497fc246bf7ba7bbe3
Jorge989 <attiejorge89@gmail.com>
2020-10-18 19:38:11 -0300

    second10

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,14 +1,14 @@
 export default [
   {
-    title: 'Já pensou em ter uma assistente pessoal sem burocracia e ganhar mais tempo ?',
+    title: 'Já pensou em ter uma assistente pessoal sem burocracia e ganhar mais tempo?',
     description:
-      'Podemos começar ? ',
+      'Podemos começar? ',
       descricao: '',
   },
   {
     title: 'Muito além de ser um diferencial, a organização pessoal garante maior eficiência e otimização do tempo, para focar no que realmente é importante.',
     description:
-      'Nosso atendimento remoto é mais econômico, sem custos fixos nem custos empregatícios, é acessível para quem não dispõe ou não deseja uma estrutura física de atendimento.Atendemos executivos, empresários, médicos, profissionais liberais e famílias.',
+      'Nosso atendimento remoto é mais econômico, sem custos fixos nem custos empregatícios, é acessível para quem não dispõe ou não deseja uma estrutura física de atendimento. Atendemos executivos, empresários, médicos, profissionais liberais e famílias.',
       descricao: '',
   },
   {
